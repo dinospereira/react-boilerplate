76c49fe031007527cd08b0bc70dc705c73233c10
Dino Pereira <dino.pereira@farfetch.com>
2020-05-21 18:01:29 +0100

    Added jest config, and workflow

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
     setupFilesAfterEnv: ['<rootDir>/testSetup.js'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/__mocks__/fileMock.js',
         '\\.(css|less)$': 'identity-obj-proxy',
     },
+    testEnvironment: 'jsdom',
+    transformIgnorePatterns: ['/node_modules/(?!.*@portal/)'],
+    testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
+    coveragePathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
 };
